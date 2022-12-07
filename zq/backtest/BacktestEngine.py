01df2a012b2d85bb82c58dd010017e44c4af39ab
dominolu <dominolu@163.com>
2022-12-07 22:47:01 +0800

    根据ChatGPT生成BacktestEngine

--- zq/backtest/BacktestEngine.py
+++ zq/backtest/BacktestEngine.py
@@ -8,74 +8,137 @@
 import numpy as np
 from typing import Callable, Dict
 
+
 # 定义BacktestEngine类
 class BacktestEngine:
-    def __init__(self, prices: np.ndarray, assets: List[str], strategy: Callable[[np.ndarray, List[str]], np.ndarray],
-                 start_date: str, end_date: str):
-        self.prices = prices
-        self.assets = assets
-        self.strategy = strategy
+    def __init__(self, data, strategy_class, strategy_params, start_date, end_date,
+                 initial_capital, commission_rate, slippage_rate):
+        """
+        :param data: {"symbol1":ndarray,"symbol2":ndarray}
+        :param strategy_class:
+        :param strategy_params:
+        :param start_date:
+        :param end_date:
+        :param initial_capital:
+        :param commission_rate:
+        :param slippage_rate:
+        """
+
+        self.data = data
+        self.strategy_class = strategy_class
+        self.strategy_params = strategy_params
         self.start_date = start_date
         self.end_date = end_date
+        self.initial_capital = initial_capital
+        self.commission_rate = commission_rate
+        self.slippage_rate = slippage_rate
 
-        # 初始化统计信息
-        self.statistics = {}
+        self.symbols = list(data.keys())
+        # 转存行情数据为 numpy 三维数组
+        self.data_matrix = np.array([data[symbol] for symbol in self.symbols])
+        # 根据参数初始化策略
+        self.strategy = self.strategy_class(**self.strategy_params)
 
-        # 初始化交易记录
-        self.trades = []
+        # 初始化撮合过程中的数据
+        data_length = self.data_matrix[0].shape[0]
+        self.positions = np.zeros((data_length, len(self.symbols)))
+        self.trades = np.zeros((data_length, len(self.symbols)))
+        self.equity = np.zeros((data_length,))
+
+    def run_strategy(self, strategy, initial_capital, commission_rate, slippage_rate):
+        # 计算因子
+        factors = strategy.factors(self.data_matrix)
+        # 循环遍历数据
+        for i in range(data_length):
+            # 获取当前时间的行情数据和因子信号
+            klines = self.data_matrix[:, i, :]
+            signals = factors[:, i]
+            # 根据行情数据和因子信号，生成交易订单
+            order = strategy.run(klines, signals)
+            # 将交易订单写入到 trades 数组中
+            self.trades[i] = order
+            # 撮合交易订单
+            trade = self.match_order(order)
+            # 更新持仓数据
+            self.positions[i] = self.positions[i - 1] + trade
+            # 计算当前时间的账户权益
+            # equity_change代表每次交易的收益，也就是成交量 * (成交价 - 持仓均价)
+            equity_change = trade * (klines[-1] - self.positions[i - 1] * klines[-1])
+            # 账户权益变化 = 每次交易的收益 + 手续费
+            equity_change -= trade * commission_rate
+            # 账户权益变化 = 账户权益变化 - 滑点损耗
+            equity_change -= trade * slippage_rate
+            # 更新权益数组
+            self.equity[i] = self.equity[i - 1] + equity_change
 
     def run(self):
-        # 调用策略函数计算交易信号
-        signals = self.strategy(self.prices, self.assets)
+        # 根据策略参数，循环实例化策略
+        for strategy_params in self.strategy_params:
+            # 实例化策略
+            strategy = self.strategy_class(**strategy_params)
+            # 调用 run_strategy 函数，计算收益率
+            performance = self.run_strategy(strategy, self.initial_capital, self.commission_rate, self.slippage_rate)
+            # 将收益率数据保存起来
+            self.performances.append(performance)
+        # 根据收益率，夏普，最大回撤排序
+        sorted_performances = sorted(self.performances, key=lambda x: x[1], reverse=True)
+        # 输出每个策略参数与相应收益率数据
+        for p in sorted_performances:
+            print(p[0], p[1])
 
-        # 遍历所有品种
-        for i in range(len(self.assets)):
-            symbol = self.assets[i]
-            signal = signals[:, i]
+    def calculate_statistics(self, equity):
+        # 计算收益率
+        returns = (equity[-1] - equity[0]) / equity[0]
+        # 计算夏普比率
+        sharpe_ratio = returns / equity.std()
+        # 计算最大回撤
+        max_drawdown = (equity.cummax() - equity) / equity.cummax()
+        # 计算年化收益率
+        annualized_return = returns / len(equity) * 252
+        # 计算盈利天数
+        win_days = equity[equity > equity.shift(1)].count()
+        # 计算亏损天数
+        lose_days = equity[equity < equity.shift(1)].count()
+        # 计算平均盈利
+        average_win = equity[equity > equity.shift(1)].mean()
+        # 计算平均亏损
+        average_lose = equity[equity < equity.shift(1)].mean()
+        # 计算盈亏比
+        win_loss_ratio = average_win / abs(average_lose)
 
-            # 计算累计收益
-            returns = calculate_returns(self.prices[:, i, :], signal)
-            # 计算年化收益率
-            annual_return = calculate_annual_return(returns, self.start_date, self.end_date)
-            # 更新统计信息
-            self.statistics[symbol] = {
-                "return": returns,
-                "annual_return": annual_return,
-            }
+        return [returns,sharpe_ratio,max_drawdown,annualized_return,win_days,lose_days,average_win,average_lose,win_loss_ratio]
 
-            # 记录交易记录
-            self.trades[symbol] = signal
 
-    def calculate_statistics(self):
-        # 计算策略的年化收益率
-
-        annual_return = (self.returns + 1).prod() ** (252 / len(self.returns)) - 1
-        self.statistics['annual_return'] = annual_return
-
-        # 计算策略的最大回撤
-        max_drawdown = (self.returns + 1).cumprod().cummax().sub(self.returns.cumprod() + 1).max()
-        self.statistics['max_drawdown'] = max_drawdown
-
-        # 计算策略的夏普比率
-        sharpe_ratio = self.returns.mean() / self.returns.std()
-        self.statistics['sharpe_ratio'] = sharpe_ratio
-
-        # 计算策略的风险
-        risk = self.returns.std() * np.sqrt(252)
-        self.statistics['risk'] = risk
-
-    def calculate_returns(self):
-        # 计算策略的收益率
-        self.returns = np.diff(self.prices) / self.prices[:-1]
-
-        # 撮合交易
-        for i in range(len(self.signals)):
-            if self.signals[i] == 1:
-                # 买入，计算交易成本
-                cost = self.prices[i] * self.commission + self.prices[i] * self.slippage
-                self.returns[i] -= cost
-            elif self.signals[i] == -1:
-                # 卖出，计算交易成本
-                cost = self.prices[i] * self.commission + self.prices[i] * self.slippage
-                self.returns[i] += cost
-
+# def calculate_statistics(self):
+#     # 计算策略的年化收益率
+#
+#     annual_return = (self.returns + 1).prod() ** (252 / len(self.returns)) - 1
+#     self.statistics['annual_return'] = annual_return
+#
+#     # 计算策略的最大回撤
+#     max_drawdown = (self.returns + 1).cumprod().cummax().sub(self.returns.cumprod() + 1).max()
+#     self.statistics['max_drawdown'] = max_drawdown
+#
+#     # 计算策略的夏普比率
+#     sharpe_ratio = self.returns.mean() / self.returns.std()
+#     self.statistics['sharpe_ratio'] = sharpe_ratio
+#
+#     # 计算策略的风险
+#     risk = self.returns.std() * np.sqrt(252)
+#     self.statistics['risk'] = risk
+#
+#
+# def calculate_returns(self):
+#     # 计算策略的收益率
+#     self.returns = np.diff(self.prices) / self.prices[:-1]
+#
+#     # 撮合交易
+#     for i in range(len(self.signals)):
+#         if self.signals[i] == 1:
+#             # 买入，计算交易成本
+#             cost = self.prices[i] * self.commission + self.prices[i] * self.slippage
+#             self.returns[i] -= cost
+#         elif self.signals[i] == -1:
+#             # 卖出，计算交易成本
+#             cost = self.prices[i] * self.commission + self.prices[i] * self.slippage
+#             self.returns[i] += cost
--- zq/backtest/main.py
+++ zq/backtest/main.py
@@ -7,8 +7,34 @@
 
 
 
-strategy = Strategy(10, 0.01)
+# 定义多品种行情数据、策略类、策略参数范围、起止日期、现金、手续费率和滑点
+data = {
+  'AAPL': prices_data,
+  'GOOG': prices_data,
+  'MSFT': prices_data
+}
+strategy_class = MovingAverageCrossStrategy
+strategy_params = {
+  'short_window': range(10, 20),
+  'long_window': range(30, 40)
+}
+start_date = '2010-01-01'
+end_date = '2018-12-31'
+initial_capital = 10000.0
+commission_rate = 0.001
+slippage_rate = 0.001
 
-# 运行回测引擎
-backtest = Backtest(prices, assets, strategy.factors, start_date, end_date)
-results = backtest.run()
+# 使用字典来传递参数
+params = {
+  'data': data,
+  'strategy_class': strategy_class,
+  'strategy_params': strategy_params,
+  'start_date': start_date,
+  'end_date': end_date,
+  'initial_capital': initial_capital,
+  'commission_rate': commission_rate,
+  'slippage_rate': slippage_rate
+}
+
+# 使用字典中的参数来构造 BacktestEngine
+engine = BacktestEngine(**params)
